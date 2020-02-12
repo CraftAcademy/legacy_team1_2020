254ca5f9e1104c2a2f03db3cc88e852e65920ff9
Carlos <carlos.delgado@hotmail.it>
2020-02-12 15:28:57 +0100

    adding oliver testing on project

--- spec/feature/user_can_create_new_account_spec.rb
+++ spec/feature/user_can_create_new_account_spec.rb
@@ -6,9 +6,42 @@ feature 'User can create a new account' do
         visit new_user_registration_path
     end
 
-    it 'displays the sign up page' do
-        expect(page).to have_field 'Name'
-        expect(page).to have_field 'Email'
-        expect(page).to have_field 'Password'
+    describe 'successfully' do
+        before do             
+            fill_in 'Name', with: 'Carlos'
+            fill_in 'Email', with: 'carlos@mail.com'
+            fill_in 'Password', with: 'password'
+            fill_in 'Password confirmation', with: 'password'
+    
+    
+            click_on 'Create'
+        end
+
+        it 'displays welcome message' do
+            expect(page).to have_content 'Hello, Carlos'
+        end
+
+        it 'redirects to root path' do
+            expect(current_path).to eq root_path
+        end
+    end
+
+    describe 'unsuccessfully' do
+        describe 'with invalid email' do
+            before do
+                fill_in 'Name', with: 'Carlos'
+                fill_in 'Email', with: 'carlosmail.com'
+                fill_in 'Password', with: 'password'
+                fill_in 'Password confirmation', with: 'password'
+        
+        
+                click_on 'Create'
+            end
+
+            it 'returns error message' do
+                expect(page).to have_content 'Email is invalid'
+            end
+
+        end
     end
 end
