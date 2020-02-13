0327b8e18a188aca1376284fd0e9b0d2994b577a
Carlos <carlos.delgado@hotmail.it>
2020-02-13 13:42:47 +0100

    adding test of links in the login page

--- spec/feature/user_can_access_mail_box_spec.rb
+++ spec/feature/user_can_access_mail_box_spec.rb
@@ -9,10 +9,12 @@ feature 'User can access inbox' do
         visit root_path
     end
 
+        #Checking Login from homepage
     it'display login' do
         expect(page).to have_content'Login'
     end
 
+        #Testing components from Log in page
     describe 'displays the login page' do
         before do      
             click_on 'Login'
@@ -35,9 +37,23 @@ feature 'User can access inbox' do
             expect(page).to have_field 'Remember me'
         end
 
-        it' display the input fields' do
+        it' display the link' do
             expect(page).to have_link('Sign up', href: new_user_registration_path)
             #it { should have_link('Help', href: help_path) }
         end
+
+        it 'display the link' do
+            expect(page).to have_link('Forgot your password?', href: new_user_password_path)
+        end
+
+        it'displays the Sign up button' do
+            expect(page).to have_link('Sign up', href: new_user_registration_path)
+        end
+
+        it 'displays the link to the home page' do
+            expect(page).to have_link('CA Mailboxer', href: root_path)
+        end
+
     end
+
 end
