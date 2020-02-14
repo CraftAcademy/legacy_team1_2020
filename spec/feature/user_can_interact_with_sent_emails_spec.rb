65d78605a5818c598f61fc89b39e0750e095e4c3
Janko <mailodjanka@gmail.com>
2020-02-14 14:41:14 +0100

    More refactoring - erased an unncessary blocks

--- spec/feature/user_can_interact_with_sent_emails_spec.rb
+++ spec/feature/user_can_interact_with_sent_emails_spec.rb
@@ -11,41 +11,33 @@ feature 'User can view sent mail and reply' do
         fill_in 'Password', with: user.password
         click_on 'Log in'
         click_on 'Inbox'
-    end
-
-    describe 'Successfully' do 
-      before do
         click_on 'Compose'
         click_on 'User'
         fill_in 'Subject', with: 'Subject sent mail'
         fill_in 'Type your message here', with: 'Message for sent mail'
         click_on 'Send Message'
         click_on 'Sent'
-      end
-
-      it 'User can view a sent email' do
+    end
+ 
+    it 'User can view a sent email' do
           expect(page).to have_content 'Subject sent mail'
       end
 
-      it 'User can navigate to Sent mails ' do
+    it 'User can navigate to Sent mails ' do
         expect(current_path).to eq mailbox_sent_path
       end
 
-        describe 'User can view the sent mail item' do 
-            before do 
-                click_on 'View'
-
-             end 
+    describe 'User can view the sent mail item' do 
+        before do 
+            click_on 'View'
+        end 
 
             #it 'User is redirected to the specific mail item' do
                 #expect(current_path).to eq conversation_path()
             #end 
 
-            it 'User can open a sent message' do
-                expect(page).to have_content 'Move to trash'
-            end 
-
+        it 'User can open a sent message' do
+            expect(page).to have_content 'Move to trash'
         end 
- 
-    end
+    end 
 end
