f0094335235d87b3bc53f634b04c0f2617d9edca
Carlos <carlos.delgado@hotmail.it>
2020-02-14 18:12:11 +0100

    create merge conflict

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
     factory :user do
         name { 'User1' }
         email { 'user1_email@gmail.com' }
-        password { 'password' }
+        password { 'hfdsg' }
     end
 end
